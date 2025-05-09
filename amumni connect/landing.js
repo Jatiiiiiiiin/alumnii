3e7d5dfe457fa2faddcb0725d0e88354eaf477da
Jatiiiiiiiin <jatinjatinth@gmail.com>
2025-05-09 11:38:35 +0530

    main

---
+++ amumni connect/landing.js
@@ -0,0 +1,15 @@
+function login() {
+    window.location.href = "login.html";
+}
+
+x.style.display = "none";
+function myFunction() {
+
+    
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
